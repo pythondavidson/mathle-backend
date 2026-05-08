23375687a441e05954e57a1df44f52ea46d43060
pythondavidson <granadossergio662@gmail.com>
2026-05-08 21:51:46 +0200

    login name

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username:          { type: String, required: true, unique: true, trim: true, minlength: 3, maxlength: 10 },
   email:             { type: String, required: true, unique: true, trim: true, lowercase: true },
+  name:              { type: String, default: null, trim: true },
   passwordHash:      { type: String, default: null },
   googleId:          { type: String, default: null },
   totalPoints:       { type: Number, default: 0 },
